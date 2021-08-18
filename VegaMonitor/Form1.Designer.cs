f3c8f9592f7d309159799f65c50c6e7cded8fc8f
Paul <pbaker@easynet.co.uk>
2021-08-18 15:32:12 +0100

    Add project files.

---
+++ VegaMonitor/Form1.Designer.cs
@@ -0,0 +1,82 @@
+﻿namespace VegaMonitor
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.monitorGridView = new System.Windows.Forms.DataGridView();
+            this.monitor_label_status = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.monitorGridView)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // monitorGridView
+            // 
+            this.monitorGridView.AllowUserToAddRows = false;
+            this.monitorGridView.AllowUserToDeleteRows = false;
+            this.monitorGridView.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.monitorGridView.AutoSizeRowsMode = System.Windows.Forms.DataGridViewAutoSizeRowsMode.DisplayedCells;
+            this.monitorGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.monitorGridView.Location = new System.Drawing.Point(12, 28);
+            this.monitorGridView.Name = "monitorGridView";
+            this.monitorGridView.ReadOnly = true;
+            this.monitorGridView.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.monitorGridView.Size = new System.Drawing.Size(623, 640);
+            this.monitorGridView.TabIndex = 0;
+            // 
+            // monitor_label_status
+            // 
+            this.monitor_label_status.AutoSize = true;
+            this.monitor_label_status.Location = new System.Drawing.Point(12, 9);
+            this.monitor_label_status.Name = "monitor_label_status";
+            this.monitor_label_status.Size = new System.Drawing.Size(15, 13);
+            this.monitor_label_status.TabIndex = 1;
+            this.monitor_label_status.Text = "**";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(647, 680);
+            this.Controls.Add(this.monitor_label_status);
+            this.Controls.Add(this.monitorGridView);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "Form1";
+            this.Text = "Vega Site Monitor v1.00";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.monitorGridView)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView monitorGridView;
+        private System.Windows.Forms.Label monitor_label_status;
+    }
+}
+
---
+++ VegaMonitor/MySQL_connect.cs
@@ -0,0 +1,158 @@
+﻿using System;
+using MySql.Data.MySqlClient;
+using System.Collections.Generic;
+using System.IO;
+
+
+
+
+
+class DBConnect
+{
+    private MySqlConnection connection;
+    private string server;
+    private string database;
+    private string uid;
+    private string password;
+
+
+    //Constructor
+    public DBConnect(bool status)
+    {
+        Initialize(status);
+    }
+
+
+
+
+    //Initialize values
+    private void Initialize(bool status)
+    {
+        if (status) {
+            database = "vegastats_live";
+            uid = "vegalive";
+        }else
+        {
+            database = "vegastats_test";
+            uid = "vegatest";
+        }
+
+        server = "217.172.143.116";
+        password = "1964Pib3";
+        string connectionString;
+        connectionString = "SERVER=" + server + ";" + "DATABASE=" + database + ";" + "UID=" + uid + ";" + "PASSWORD=" + password + ";";
+
+        connection = new MySqlConnection(connectionString);
+    }
+
+
+
+    //open connection to database
+    private bool OpenConnection()
+    {
+        try
+        {
+            connection.Open();
+            return true;
+        }
+        catch (MySqlException ex)
+        {
+            //When handling errors, you can your application's response based 
+            //on the error number.
+            //The two most common error numbers when connecting are as follows:
+            //0: Cannot connect to server.
+            //1045: Invalid user name and/or password.
+            switch (ex.Number)
+            {
+                case 0:
+                    Console.WriteLine("Cannot connect to server.  Contact administrator");
+                    break;
+
+                case 1045:
+                    Console.WriteLine("Invalid username/password, please try again");
+                    break;
+            }
+            return false;
+        }
+    }
+
+
+
+    //Close connection
+    private bool CloseConnection()
+    {
+        try
+        {
+            connection.Close();
+            return true;
+        }
+        catch (MySqlException ex)
+        {
+            Console.WriteLine(ex.Message);
+            return false;
+        }
+    }
+
+
+
+    //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+    public class FileNameHash
+    {
+        public string site_id { get; set; }
+        public string ping { get; set; }
+        public string last_trigger { get; set; }
+        public string localdown { get; set; }        
+    }
+
+
+
+    public List<FileNameHash> getMonitorResults(){
+
+        var sites = new List<FileNameHash>();
+
+        var thedate = DateTime.Today.ToString("yyyy-MM-dd");
+
+        //Open connection
+        if (this.OpenConnection() == true)
+        {
+            string query = "SELECT photos_taken.pk_id, photos_taken.site_id, sites.site_name, photos_taken.last_trigger, photos_taken.wait_for, photos_taken.webservice_status, -sites.timezone AS timezone, IF(photos_alert.offline_alert > 0, 'No Pulse', 'Active') AS offline, IF(photos_alert.localhost_alert > 0, 'Down ↓', 'Running ↑') AS localdown " + 
+"FROM `photos_taken` " +
+"JOIN sites ON sites.site_id = photos_taken.site_id " +
+"JOIN photos_alert ON photos_alert.fk_id = photos_taken.pk_id " +
+"WHERE photos_taken.photo_date = '"+ thedate + "'  " + 
+"ORDER BY offline, localdown, photos_taken.site_id";
+            
+            //Create Command
+            MySqlCommand cmd = new MySqlCommand(query, connection);
+
+            //Create a data reader and Execute the command
+            MySqlDataReader dataReader = cmd.ExecuteReader();
+
+            
+            while (dataReader.Read())
+            {
+                sites.Add(new FileNameHash
+                {
+                    site_id = dataReader["site_id"].ToString(),
+                    ping = dataReader["offline"].ToString(),
+                    last_trigger = dataReader["last_trigger"].ToString(),
+                    localdown = dataReader["localdown"].ToString()
+                });
+            }
+
+            //close Data Reader
+            dataReader.Close();
+        }
+
+        //close Connection
+        this.CloseConnection();
+
+        return sites;
+    }
+
+
+    
+
+
+}// end of class
